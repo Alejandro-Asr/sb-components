27178411ca303b714212d15f212a94f73d3b7c08
Alejandro <alejandro@asr-oss.com>
2021-12-22 23:31:59 +0100

    feat: add semantic-versioning

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+import './mylabel.css';
+export interface MyLabelProps {
+    /**
+     * Contenido del Label
+    */
+    label: string;
+    /**
+     * Tamaño de la etiqueta
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Capitalizar label
+     */
+    allCaps?: boolean;
+    /**
+     * Color label
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Color personalizado de la fuente
+     */
+    fontColor?: string;
+    /**
+     * Color personalizado de la fuente
+     */
+    backgroundColor?: string;
+}
+export declare const MyLabel: ({ label, size, allCaps, color, fontColor, backgroundColor }: MyLabelProps) => JSX.Element;
